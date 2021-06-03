7948e252ba21984302f3281bf87abf9fc9370ef9
Omer Degirmenci <rumidegirmenci0@gmail.com>
2021-06-03 16:29:46 +0200

    Co-authored-by: Dayan Jonker <dayanjonker@hotmail.com>

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import path from 'path'
 import connectDB from './config/db.js'
 import { getQuestions } from './controllers/questController.js'
 import { postSession } from './controllers/sessionController.js'
@@ -7,6 +8,8 @@ import { postSession } from './controllers/sessionController.js'
 const app = express()
 
 app.use(express.json())
+
+const __dirname = path.resolve()
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
@@ -28,9 +31,17 @@ const PORT = process.env.PORT || 8000
 app.get('/', getQuestions)
 app.post('/quest/session', postSession)
 
-app.get('*', (req, res) => {
-  res.status(404).json({ error: 'Not found' })
-})
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, './client/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  )
+} else {
+  app.get('*', (req, res) => {
+    res.send('API is running...')
+  })
+}
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
