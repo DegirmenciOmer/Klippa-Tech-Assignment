547ad6b5ec5083c368aa8927f8369f73b853f6e4
Omer Degirmenci <rumidegirmenci0@gmail.com>
2021-06-03 22:11:38 +0200

    deploy site cors

--- server.js
+++ server.js
@@ -12,7 +12,10 @@ app.use(express.json())
 const __dirname = path.resolve()
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
+  res.setHeader(
+    'Access-Control-Allow-Origin',
+    'https://klippa-calculation-game.netlify.app/'
+  )
   res.setHeader('Access-Control-Allow-Credentials', 'true')
   res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT')
   res.setHeader(
